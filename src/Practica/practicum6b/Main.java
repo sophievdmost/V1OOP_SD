d877b4d07b8abe2f25189894b20b8149724f730d
arvid <arvid.smit@xs4all.nl>
2021-03-18 16:51:55 +0100

    Test voor merge

--- src/Practica/practicum6b/Main.java
+++ src/Practica/practicum6b/Main.java
@@ -28,5 +28,11 @@ public class Main {
         teKoop.add(new Game("Mario Kart 8", 2019, 35.00));
         ArrayList<Game> nogNietInBezit = p1.bepaalGamesNietInBezit(teKoop);
         System.out.println("p1 heeft de volgende games nog niet: " + nogNietInBezit.toString());
+
+
+
+
+
+
     }
 }
